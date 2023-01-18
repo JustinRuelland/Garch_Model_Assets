0925a4da3ae4761d2db0771781308167348841e7
Louis Geist <louisjjgeist@gmail.com>
2023-01-18 16:15:38 +0100

    Condition_stationnarite; add ggplot2 and remove of parameters;v1

--- condition_stationnarite.R
+++ condition_stationnarite.R
@@ -4,9 +4,9 @@
 
 ### Utilisation de la fonction :
 # Mettre en paramètres la fonction de simulation et ses paramètres
-# Par exemple "f = rnorm", et les paramètres 0 et 1
+# Par exemple "loi_eta = rnorm", et les paramètres 0 et 1
 
-cond_statio <- function(f,param1,param2){
+condition_stationnarite <- function(loi_eta){
   N = 400 #nombre de alphas
   M = 10**4 #nombre de betas (ne pas dépasser 10**4 à priori)
   nsimu= 10**5 #nombre de simulations
@@ -17,7 +17,7 @@ cond_statio <- function(f,param1,param2){
   
   for(i in range(nsimu)){
     #on évalue tous les alphas et betas sur une simulation
-    xx = array(f(N*M,param1,param2), dim=c(N,M)) 
+    xx = array(loi_eta(N*M), dim=c(N,M)) 
     y = y + log(t(a*xx**2)+b)/nsimu #on ajoute à la moyenne le résultat
   }
   
@@ -28,6 +28,7 @@ cond_statio <- function(f,param1,param2){
   
   df = cbind(a,y)
   df = as.data.frame(df)
+  
   Graph_condi_satio = ggplot(data = df) + geom_line(aes(x = a,y = y))+scale_x_continuous("Alpha") + scale_y_continuous("Beta")
   plot(Graph_condi_satio)
   
