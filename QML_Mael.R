97f20233d0b05cfe82debff6fca8ce8920a426ee
mduv31 <mael.duvergertlse@gmail.com>
2023-01-20 21:02:35 +0100

    Update de la fonction pour prévision

--- QML_Mael.R
+++ QML_Mael.R
@@ -9,28 +9,25 @@ sigma2_0 = omega_0/(1-alpha_0-beta_0)
 
 #---------------------- calcul des sigma2 et des eps2 ------------------------
 
-#calcul récurssif pour un sigma(t) précis
+#calcul récurssif pour un sigma(t) précis (FACULTATIF)
 func_sigma2 <- function(t,sigma2_init,eta2,theta){
   res <- sigma2_init
   if(t>1){
     res<-theta[1] + (theta[2]*eta2[t-1] + theta[3])*func_sigma2(t-1,sigma2_init,theta)}
   return(res)}
 
-#calcul d'une suite de n sigma(t)**2, à partir de la formule récurrente
-func_suite_sigma2 <- function(n,sigma2_init,eta2,theta){
+#calcul d'une suite de n epsilon(t)**2 (à partir des sigmas(t))
+simu_eps2 <- function(n,eps2_init,sigma2_init,theta){
+  eta2 = rnorm(n,mean=0,sd=1)**2
   sigmas2 = c(sigma2_init)
   for(i in 2:n){sigmas2[i] = theta[1] + (theta[2]*eta2[i-1] + theta[3])*sigmas2[i-1]}
-  return(sigmas2)}
-
-#calcul d'une suite de n epsilon(t)**2 (à partir des sigmas(t))
-func_suite_eps2 <- function(n,eps2_init,sigma2_init,eta2,theta){
-  res = eta2*func_suite_sigma2(n,sigma2_init,eta2,theta)
-  res[0] = eps2_init
-  return(res)}
+  eps2 = eta2*sigmas2
+  eps2[0] = eps2_init
+  return(eps2)}
 
 #---------------------- Methode du quasi-maximum de vraisemblance ----------
-QML <-function(theta,n){
-  eps2 = func_suite_eps2(n,eps2_0,sigma2_0,rnorm(n,mean=0,sd=1)**2,theta)
+QML <-function(eps2){
+  n = length(eps2)
   
   f_opt <- function(theta_opt){
     sigmas2_QML = c(theta_opt[1]/(1-theta_opt[2]-theta_opt[3]))
@@ -44,7 +41,7 @@ QML <-function(theta,n){
   return(constrOptim(theta=theta_init,f = f_opt,ci=ci,ui=ui,gr=NULL))}
 
 res = matrix(0,100,3)
-for(i in 1:100){res[i,]=QML(theta_0,10**3)$par}
+for(i in 1:100){res[i,]=QML(simu_eps2(10**3,eps2_0,sigma2_0,theta_0))$par}
 res = as.data.frame(res)
 colnames(res) = c("omega","alpha","beta")
 res$omega = res$omega-omega_0
