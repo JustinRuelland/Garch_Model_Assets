0bc54dd2c7c7d7da8caa958c34b4af23e25dd3ba
mduv31 <mael.duvergertlse@gmail.com>
2023-01-21 22:28:21 +0100

    debut normalite asymptotique

--- QML_Mael.R
+++ QML_Mael.R
@@ -4,7 +4,7 @@ alpha_0 <- 0.12
 beta_0 <- 0.82
 theta_0 = c(omega_0,alpha_0,beta_0)
 eps2_0 = 0 
-#sigma2_0 = omega_0/(1-alpha_0-beta_0)
+sigma2_0 = omega_0/(1-alpha_0-beta_0)
 
 #---------------------- calcul des sigma2 et des eps2 ------------------------
 
@@ -15,35 +15,43 @@ func_sigma2 <- function(t,sigma2_init,eta2,theta){
     res<-theta[1] + (theta[2]*eta2[t-1] + theta[3])*func_sigma2(t-1,sigma2_init,theta)}
   return(res)}
 
-#calcul d'une suite de n epsilon(t)**2 (à partir des sigmas(t))
+#simulation d'une suite de n epsilon**2 à partir des sigmas**2 aussi simulés
 simu_eps2 <- function(n,eps2_init,sigma2_init,theta){
-  eta2 = rnorm(n,mean=0,sd=1)**2
+  eta2 = rnorm(n,mean=0,sd=1)**2 #eta
   sigmas2 = c(sigma2_init)
-  for(i in 2:n){sigmas2[i] = theta[1] + (theta[2]*eta2[i-1] + theta[3])*sigmas2[i-1]}
-  eps2 = eta2*sigmas2
+  for(i in 2:n){sigmas2[i] = theta[1] + (theta[2]*eta2[i-1] + theta[3])*sigmas2[i-1]} #sigma
+  eps2 = eta2*sigmas2 #epsilon
   eps2[0] = eps2_init
   return(eps2)}
 
-simu_sigma2 <- function(sigma2_init,eps2,theta){
+#simulation des sigmas**2 à partir des epsilon**2 connus
+simu_sigma2 <- function(eps2,theta){
   n = length(eps2)
-  sigmas2 = c(sigma2_init)
+  sigmas2 = c(mean(eps2))
   for(i in 2:n){sigmas2[i]=theta[1]+theta[2]*eps2[i-1]+theta[3]*sigmas2[i-1]}
   return(sigmas2)}
 
 #---------------------- Methode du quasi-maximum de vraisemblance ----------
+
+#on part du principe qu'on a observe une série financière
+#dont les rendements au carré sont eps2
 QML <-function(eps2){
-  n = length(eps2)
+  n = length(eps2) #longueur de la série financière
   
+  #fonction de vraisemblance à optimiser
   f_opt <- function(theta_opt){
-    init = mean(eps2) #theta_opt[1]/(1-theta_opt[2]-theta_opt[3]) ALTERNATIVE
-    sigmas2_QML = simu_sigma2(init,eps2,theta_opt)
+    sigmas2_QML = simu_sigma2(eps2,theta_opt) #simulation des sigmas à partir des eps2
     #on retire les 20 premières données (négligeables cf notes)
-    return(sum(log(sigmas2_QML[25:n])+(eps2[25:n]/sigmas2_QML[25:n]))) } 
+    return(sum(log(sigmas2_QML[25:n])+(eps2[25:n]/sigmas2_QML[25:n]))) } #log vraisemblance
   
-  theta_init = c(0.01,0.1,0.8)
+  theta_init = c(0.01,0.1,0.8) #valeur initiale
+  
+  #contraintes
   ui <- cbind(c(1,-1,0,0,0,0),c(0,0,1,-1,0,0),c(0,0,0,0,1,-1))
   ci <- c(0.001, -1, 0, -3, 0, -0.99)
-  return(constrOptim(theta=theta_init,f = f_opt,ci=ci,ui=ui,gr=NULL))}
+  return(constrOptim(theta=theta_init,f = f_opt,ci=ci,ui=ui,gr=NULL))} #opti sous contraintes linéaires
+
+#-----------------boxplots---------------------------
 
 #res = matrix(0,100,3)
 #for(i in 1:100){res[i,]=QML(simu_eps2(10**3,eps2_0,sigma2_0,theta_0))$par}
@@ -55,3 +63,24 @@ QML <-function(eps2){
 #boxplot(res$omega)
 #boxplot(res$alpha)
 #boxplot(res$beta)
+
+
+#----------------normalité asymptotique----------------------
+n = 10**4
+res = matrix(0,100,3)
+for(i in 1:100){res[i,]=QML(simu_eps2(n,eps2_0,sigma2_0,theta_0))$par}
+res = as.data.frame(res)
+colnames(res) = c("omega","alpha","beta")
+res$omega = sqrt(n)*(res$omega-omega_0)
+res$alpha = sqrt(n)*(res$alpha-alpha_0)
+res$beta = sqrt(n)*(res$beta-beta_0)
+plot.new() 
+hist(res$beta, breaks = 15, col = "steelblue", frame = FALSE)
+
+install.packages("pracma")
+library(pracma)
+
+s_estim= sd(res$beta)
+
+par(new = T)
+plot(dnorm(linspace(-5, 5, n = 100),mean=0,sd=s_estim))
