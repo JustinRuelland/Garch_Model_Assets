9b23875f9be2c3976ba16275ebb490de57505830
mduv31 <mael.duvergertlse@gmail.com>
2023-01-28 12:22:44 +0100

    modification backtest à la suite du rdv4 v2

--- prevision.R
+++ prevision.R
@@ -1,19 +1,21 @@
-
 #-----------------Backtests--------------------
 
-func_backtest <- function(eps2,q_inf,q_sup,cut){
+func_backtest <- function(eps,q_inf,q_sup,cut){
   
-  #QML
-  source(file = "C:/Users/maeld/OneDrive/Documents/GitHub/Garch_Model_Assets/QML_Mael.R",local= TRUE)
-  n = length(eps2)
+  n = length(eps)
+  eps2 = eps**2
+  
+  #QML, on s'attend à ce que la fonction ait été importée
   theta = QML(eps2[c(1:cut)]) #estimation de theta par QML sur les données jusqu'au "cut"
   sigma2 = simu_sigma2(eps2,theta) #estimation des sigma2 à partir des epsilon2 passés
+  sigma = sqrt(sigma2)
   
   #tests intervalle de confiance
   cut = cut+1
-  vect_test = sqrt(eps2/sigma2)[cut:n]
-  outside = length(vect_test[(vect_test>q_sup)|(vect_test<q_inf)]) #REVOIR
-  inside = length(vect_test) - outside
+  vect_test = eps[cut:n]/sigma[cut:n]
+  test_sigma = sigma[cut:n]
+  outside = length(vect_test[(vect_test>q_sup)|(vect_test<q_inf)])
+  inside = n - cut + 1 -outside
 
   #test d'adéquation du khi2 sur les Bernoulli (paramètre 0.95) (REVOIR Binomiale)
   obs = c(outside,inside) #effectifs observés
