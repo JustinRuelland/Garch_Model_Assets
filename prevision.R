677f7e06907e7f13f75350e7d7ff8a3405c3c225
mduv31 <mael.duvergertlse@gmail.com>
2023-01-30 21:18:42 +0100

    prevision.R, création fonction prévision des carrés, v2

--- prevision.R
+++ prevision.R
@@ -24,11 +24,35 @@ func_backtest <- function(eps,q_inf,q_sup,cut){
   obs = c(outside,inside) #effectifs observés
   proba = c(0.05,0.95) #probabilités théoriques
   
-  p = chisq.test(obs,p=proba)$p.value
   res = matrix(0,ncol=3)
   colnames(res) = c("p.value","upper.bounds","lower.bounds")
-  res$p.value = p
+  res$p.value = chisq.test(obs,p=proba)$p.value
   res$upper.bounds = upper
   res$lower.bounds = lower
 
+  return(res)}
+
+#hypothèse: les etas suivent une loi normale
+prevision_square <-function(eps2,cut){
+  
+  n = length(eps2)
+  theta = QML(eps2[c(1:cut)]) #estimation de theta par QML sur les données jusqu'au "cut"
+  sigma2_hat = simu_sigma2(eps2,theta)
+  
+  cut = cut+1
+  eps2_test = eps2[cut:n]
+  sigma2_test = sigma2_hat[cut:n]
+  upper = qchisq(df=1,p=0.95)*sigma2_test
+  outside = length(eps2_test[(eps2_test>upper)])
+  inside = n - cut + 1 -outside
+  
+  #test d'adéquation du khi2 sur les Bernoulli (paramètre 0.95) (REVOIR Binomiale)
+  obs = c(outside,inside) #effectifs observés
+  proba = c(0.05,0.95) #probabilités théoriques
+  
+  res = matrix(0,ncol=2)
+  colnames(res) = c("p.value","upper.bounds")
+  res$p.value = chisq.test(obs,p=proba)$p.value
+  res$upper.bounds = upper
+  
   return(res)}
