81e830dd30fd07d8c4b1be023f235463a74d4512
Louis GEIST <louisjjgeist@gmail.com>
2023-01-31 12:55:16 +0100

    main_Geist.R ; test sur chgt de GARCH (pas concluant) ; alpha

--- Geist/main_Geist.R
+++ Geist/main_Geist.R
@@ -1,44 +1,67 @@
 ###--------------- Puissance du test --------------------
 # Pas le calcul de la puissance du test, 
-# mais vérification pour des cas précis que le test rejette bien 
+# mais vérification pour des cas précis que le test rejette bien
 
+library("dplyr")
+library("purrr")
+library("plyr")
+library("ggplot2")
+
+setwd("C:/Users/louis/Garch_Model_Assets")
 source(file="./simulation_series.R")
-source(file= "./QML_Variance.R",local=TRUE)
+source(file= "./QML_Variance.R")
 source(file= "./prevision.R",local=TRUE)
 
 # Changement de GARCH
-n = 5000 #nombres de jours
-theta2 = c(0.0001,0.12,0.85)
-theta2 = c(0.0001,0.12,0.85)
+n = 2000 #nombres de jours
+theta1 = c(0.0001,0.12,0.85)
+
+N = 30 #nombre de alphas
+M = 30 #nombre de betas 
+
+a = seq(0.0,1,length.out=N)
+b = seq(0.0,1,length.out=M)
+couple = cross2(a,b)
 
 
-N = 400 #nombre de alphas
-M = 10**4 #nombre de betas 
+f_epaule <- function(alpha_beta){
+  rendements = simulation_rendements_avec_changement_GARCH(n,theta1,unlist(c(0.0001,c(alpha_beta))))
+  p_val = func_backtest(rendements,-1.96,1.96,8*n%/%10,FALSE)$p.value
+  return(p_val)
+}
 
-a = seq(0.01,0.2,length.out=N)
-b = seq(0.6,0.98,length.out=M)
-y = array(0,dim=c(M,N))
+tab_alpha = c()
+tab_beta = c()
 
-mes_alpha = c()
-mes_beta = c()
-mes_p_val = c()
+long = N*M
+for(i in 1:long){
+  tab_alpha[i] = c(couple[[i]][[1]])
+  tab_beta[i] = c(couple[[i]][[2]])
+}
 
-for(alpha in a){
-  print(alpha)
-  for(beta in b){
-    theta2 = c(0.0001,alpha,beta)
-    simulation_rendements_avec_changement_GARCH(5000,theta1,theta2)
-    
-    p_val = func_backtest(rendements,-1.96,1.96,8*n%/%10)$p.value
-    if(p_val>0){
-      mes_alpha = c(mes_alpha,alpha)
-      mes_beta = c(mes_beta,beta)
-      mes_p_val = c(mes_p_val,p_val)
-    }
+
+tab_pval = laply(couple,f_epaule)
+
+### Echelonnage des p-valeurs
+echelon_pval<-function(pval){
+  if(pval<0.05){
+    return(0)
+  }
+  else if((0.05<=pval)&(pval<0.1)){
+    return(0.05)
+  }
+  else{
+    return(1)
   }
 }
 
-M=as.data.frame(y)
+tab_pval_echelon = laply(tab_pval,echelon_pval)
 
-p = ggplot(data = y)+geom_bin2d()
+
+### Affichage
+# Transformation données
+df = as.data.frame(cbind(tab_alpha,tab_beta,tab_pval_echelon))
+
+
+p = ggplot(data = df,aes(x = tab_alpha,y=tab_beta,weight=tab_pval_echelon))+geom_bin2d( )
 p
