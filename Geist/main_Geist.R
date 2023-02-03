868a8e1d8c3f513653e8b3a6aec72be8aeb1d84d
Louis GEIST <louisjjgeist@gmail.com>
2023-02-03 21:23:30 +0100

    main_Geist.R ; carte bleue : moyenne des rejets de test ; alpha

--- Geist/main_Geist.R
+++ Geist/main_Geist.R
@@ -13,18 +13,20 @@ source(file= "./QML_Variance.R")
 source(file= "./prevision.R",local=TRUE)
 
 #--------------------------- Changement de GARCH --------------------------------
+niveau_test = 0.05
+
 n = 1000 #nombres de jours
 theta1 = c(0.0001,0.12,0.85)
 
-N = 30 #nombre de alphas
-M = 30 #nombre de betas 
+N = 10 #nombre de alphas
+M = 10 #nombre de betas 
 
-a = seq(0.0,0.3,length.out=N)
-b = seq(0.6,1,length.out=M)
+a = seq(0.0,1,length.out=N)
+b = seq(0.0,1,length.out=M)
 couple = cross2(a,b)
 
 
-f_epaule <- function(alpha_beta){
+test_for_alphabeta <- function(alpha_beta){
   rendements = simulation_rendements_avec_changement_GARCH(n,theta1,unlist(c(0.0001,c(alpha_beta))))
   p_val = func_backtest(rendements,-1.96,1.96,8*n%/%10,FALSE)$p.value
   return(p_val)
@@ -40,28 +42,28 @@ for(i in 1:long){
 }
 
 
-tab_pval = laply(couple,f_epaule)
+tab_pval = laply(couple,test_for_alphabeta)
 
-### Echelonnage des p-valeurs
-echelon_pval<-function(pval){
-  if(pval<0.05){
-    return(0)
-  }
-  else if((0.05<=pval)&(pval<0.1)){
-    return(0.05)
-  }
-  else{
-    return(1)
-  }
+n_traj =20
+
+tab_res_test = rep(0,N*M)
+for(j in 1:n_traj){
+  tab_pval = laply(couple,test_for_alphabeta)
+  
+  # Syntaxe pour associer la valeur 1 si la pvaleur est supérieure au niveau, 0 sinon
+  tab_pval[tab_pval>niveau_test]=1
+  tab_pval[tab_pval<=niveau_test]=0
+  
+  tab_res_test = tab_res_test+tab_pval
 }
-# A appliquer si on veut connaître que des tranches de p-valeur
-#tab_pval = laply(tab_pval,echelon_pval)
+
+tab_res_test = tab_res_test /n_traj #pour obtenir moyenne
 
 
 ### Affichage
 # Transformation données
-df = as.data.frame(cbind(tab_alpha,tab_beta,tab_pval))
+df = as.data.frame(cbind(tab_alpha,tab_beta,tab_res_test))
 
 
-p = ggplot(data = df,aes(x = tab_alpha,y=tab_beta,weight=tab_pval))+geom_bin2d( )
+p = ggplot(data = df,aes(x = tab_alpha,y=tab_beta,weight=tab_res_test))+geom_bin2d( )
 p
