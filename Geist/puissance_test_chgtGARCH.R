615d5e049fc04abf77eb64fd085fafa0239d899b
Louis GEIST <louisjjgeist@gmail.com>
2023-02-09 18:51:28 +0100

    puissance_test_chgtGARCH.R ; changes to plot new graphics ; beta

--- Geist/puissance_test_chgtGARCH.R
+++ Geist/puissance_test_chgtGARCH.R
@@ -23,21 +23,21 @@ Rprof()
 #--------------------------- Changement de GARCH ------------------------------
 niveau_test = 0.05
 n = 1000 #nombres de jours
-n_path = 10 #nombres de trajectoires simulées pour chaque carré
+n_path = 25 #nombres de trajectoires simulées pour chaque carré
 
 theta1 = c(0.0001,0.12,0.85)
 
 N = 30 #nombre de alphas
 M = 30 #nombre de betas 
 
-a = seq(0.0,0.4,length.out=N)
-b = seq(0.7,1,length.out=M)
+a = seq(0.0,1,length.out=N)
+b = seq(0,1,length.out=M)
 couple = cross2(a,b)
 
 
 test_for_alphabeta <- function(alpha_beta){
   rendements = simulation_rendements_avec_changement_GARCH(n,theta1,unlist(c(0.0001,c(alpha_beta))),0.8)
-  p_val = func_backtest(rendements,-1.96,1.96,4*n%/%10,FALSE)$p.value #8*n%/%10 : le cut est à 80% des données
+  p_val = func_backtest(rendements,-1.96,1.96,8*n%/%10,FALSE)$p.value #8*n%/%10 : le cut est à 80% des données
   return(p_val)
 }
 
@@ -58,13 +58,13 @@ for(j in 1:n_path){
   tab_pval = laply(couple,test_for_alphabeta)
   
   # Syntaxe pour associer la valeur 1 si la pvaleur est supérieure au niveau, 0 sinon
-  tab_pval[tab_pval>niveau_test]=1
-  tab_pval[tab_pval<=niveau_test]=0
+  tab_pval[tab_pval>niveau_test] = 1
+  tab_pval[tab_pval<=niveau_test] = 0
   
   tab_res_test = tab_res_test+tab_pval
 }
 
-tab_res_test = tab_res_test /n_path #pour obtenir moyenne
+tab_res_test = tab_res_test /n_path # pour obtenir moyenne
 
 
 ### Affichage
