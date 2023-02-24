25cdaa0e533dbb354b09584547962295021ea424
Louis GEIST <louisjjgeist@gmail.com>
2023-02-24 17:08:33 +0100

    data_preparation.R ; add of transform_csv_with_discounnt(data,r) to take into account risk free rate ; beta

--- data_preparation.R
+++ data_preparation.R
@@ -1,3 +1,5 @@
+library (tidyverse)
+
 transform_csv <- function(data){
   data = select(data, "Open","Date")
   data = rename(data, c("Prix"="Open"))
@@ -24,3 +26,31 @@ autocorrelations <-function(data){
   acf(data$rendement,type='correlation', na.action=na.pass, plot=TRUE)
   acf(data$rendement2,type='correlation', na.action=na.pass, plot=TRUE)
 }
+
+# New
+
+transform_csv_with_discount <- function(data,r){ #similar as transform_csv
+  data = select(data, "Open","Date")
+  data = rename(data, c("Prix"="Open"))
+  
+  data$Prix = as.numeric(data$Prix)
+  data$Date = as.Date(data$Date)
+  
+  # Discounting module
+  n = length(data$Prix)
+  discount_vector = double(n)
+  discount_vector[1]=1
+  for(i in 2:n){
+    discount_vector[i] = discount_vector[i-1]*(1+r)
+  }
+  data$Prix = data$Prix / discount_vector
+  
+  # End of transform_csv
+  data = mutate(data, rendement = log(data$Prix/lag(data$Prix)))
+  data = mutate(data, rendement2 = rendement**2)
+  return(data[-1,])
+  
+  
+  
+}
+
