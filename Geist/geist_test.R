060102cd758f4d4b244da87c5496f2c83887509a
Louis GEIST <louisjjgeist@gmail.com>
2023-03-01 21:11:34 +0100

    Geist/ ; test change, nothing special ; beta

--- Geist/geist_test.R
+++ Geist/geist_test.R
@@ -1,8 +1,12 @@
 source(file = "./data_preparation.R",local= TRUE)
-data = read.csv("./CAC40_15_19.csv") 
-#data = read.csv("./^GDAXI.csv")
+#data = read.csv("./CAC40_15_19.csv") 
+data = read.csv("./^GDAXI.csv")
 
-data_disc = transform_csv_with_discount(data,0.01/365)
+data_disc = transform_csv_with_discount(data,3/365)
+
+rendement = data_disc$rendement
+rendement = rendement[!is.na(rendement)]
+
 plot_series_temp(data_disc)
 
 data_no_discount = transform_csv(data)
@@ -15,10 +19,10 @@ source(file= "./prevision.R",local=TRUE)
 
 rendements_disc = c(data_disc$rendement) # si on met c() ou pas, ça change la taille du vecteur -> comprends pas
 n = length(rendements_disc)
-p_val_disc = func_backtest(rendements_disc,-1.96,1.96,8*n%/%10,FALSE)$p.value
+p_val_disc = func_backtest(rendement,-1.96,1.96,8*n%/%10,FALSE)$p.value
 
 rendements_no_disc = c(data_no_discount$rendement)
-p_val_no_disc = func_backtest(rendements_no_disc,-1.0,1.0,8*n%/%10,FALSE)$p.value
+p_val_no_disc = func_backtest(rendements_no_disc,-1.96,1.96,8*n%/%10,FALSE)$p.value
 
 
 rendements_disc[200]
@@ -27,4 +31,5 @@ rendements_no_disc[200]
 print(p_val_disc == p_val_no_disc)
 
 # Pb pour DAX : la fonction d'optimisation bug car série trop longue
-# CAC40 : p_valeur nickel pour
+# CAC40 : p_valeur : 0.68 pour série discountée
+#                    10^-24 pour série non discountée
