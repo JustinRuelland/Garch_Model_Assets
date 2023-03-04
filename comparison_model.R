39ffca2e91bdb5e54a0eb10b7cc1aa40bfe89df0
mduv31 <mael.duvergertlse@gmail.com>
2023-03-04 15:01:53 +0100

    Comparison_model.R, première version, peu robuste, à revoir

--- comparison_model.R
+++ comparison_model.R
@@ -1,8 +1,5 @@
 rm(list=ls())
 
-install.packages("forecast")
-library(forecast)
-
 install.packages("signal")
 install.packages("tidyverse") #contient notamment ggplot2, dplyr
 install.packages("lubridate")
@@ -13,31 +10,59 @@ library(dplyr)
 library(ggplot2)
 library(lubridate)
 
+
+library(zoo)
+install.packages("forecast")
+library(forecast)
+
+
 source(file= "./QML_Variance.R",local=TRUE)
 source(file = "./data_preparation.R",local= TRUE)
 data = read.csv("./CAC40_15_19.csv") #fichier csv de Yahoo finance
 data <- transform_csv(data)
 
-test_mariano <- function(pred1,pred2,val,loss,hor){
+test_mariano <- function(pred1,pred2,val,hor){
   e1 = pred1-val
   e2 = pred2-val
   test =  dm.test(pred1-val, pred2-val, 
               alternative = c("two.sided"), h = hor, power = 2)
   
-  res=data.frame(X=c(test.p.value,test.statistic))
-  Y=c('p_value','stat_test')
-  res$Y=Y[res$X]
-  
-  return(res)}
+  return(test)}
 
 pred_h1_garch <- function(eps2,cut){
+  
   n = length(eps2)
-  theta =  QML(eps2[1:floor(n*cut)])
-  pred = c()
-  pred[1] = theta[1]/(1-theta[2]-theta[3])
-  for(i in 2:n-floor(n*cut)-1){
-    print(eps2[floor(n*cut)+i-1])
-    pred[i] = theta[1]+theta[2]*eps2[floor(n*cut)+i-1]+theta[3]*pred[i-1]}
+  n_cut = floor(n*cut)
+  
+  theta =  QML(eps2[1:n_cut])
+  pred = double(n-n_cut)
+  init = theta[1]/(1-theta[2]-theta[3])
+  pred[1] = func_sigma2(n_cut,init,eps2[1:n_cut],theta)
+  
+  l = n-n_cut
+  for(i in 2:l){
+    pred[i] = theta[1]+theta[2]*eps2[n_cut+i-2]+theta[3]*pred[i-1]}
   return(pred)}
 
-pred_h1_garch(data$rendement2,0.8)
+
+rolling_av <- function(eps2,cut){
+  
+  n = length(eps2)
+  n_cut = floor(n*cut)
+  res_temp = rollmean(x=c(rep(NA,9),eps2),k = 10, align='right',na.rm = TRUE)
+  print(res_temp)
+  s = n_cut+1
+  return(res_temp[s:length(res_temp)])}
+
+
+
+l = floor(0.8*length(data$rendement2))
+yy_garch = pred_h1_garch(data$rendement2,0.8)
+plot(data$rendement2[l:length(data$rendement2)],type='l')
+lines(c(1:205),yy_garch, col='red')
+
+plot(data$rendement2[l:length(data$rendement2)],type='l')
+yy_roll = rolling_av(data$rendement2,0.8)
+lines(c(1:205),yy_roll, col='red')
+
+test_mariano(yy_garch,yy_roll,data$rendement2[l:length(data$rendement2)],hor=1)
