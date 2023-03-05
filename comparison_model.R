4dd23fa0618c76e41babc4406d67256160c33a6f
mduv31 <mael.duvergertlse@gmail.com>
2023-03-05 11:02:14 +0100

    Comparison_model.R, finalisation des fonctions, V3, reste à commenter le code

--- comparison_model.R
+++ comparison_model.R
@@ -1,33 +1,8 @@
-rm(list=ls())
-
-install.packages("signal")
-install.packages("tidyverse") #contient notamment ggplot2, dplyr
-install.packages("lubridate")
-
-library(signal)
-library (tidyverse)
-library(dplyr)
-library(ggplot2)
-library(lubridate)
-
-
-library(zoo)
-install.packages("forecast")
-library(forecast)
-
-
-source(file= "./QML_Variance.R",local=TRUE)
-source(file = "./data_preparation.R",local= TRUE)
-data = read.csv("./CAC40_15_19.csv") #fichier csv de Yahoo finance
-data <- transform_csv(data)
-
 test_mariano <- function(pred1,pred2,val,hor){
   e1 = pred1-val
   e2 = pred2-val
-  test =  dm.test(pred1-val, pred2-val, 
-              alternative = c("two.sided"), h = hor, power = 2)
-  
-  return(test)}
+  return(dm.test(pred1-val, pred2-val, alternative = c("two.sided"), h = hor, power = 2))}
+
 
 pred_h1_garch <- function(eps2,cut){
   
@@ -35,34 +10,20 @@ pred_h1_garch <- function(eps2,cut){
   n_cut = floor(n*cut)
   
   theta =  QML(eps2[1:n_cut])
-  pred = double(n-n_cut)
+  pred = double(n-n_cut+1)
   init = theta[1]/(1-theta[2]-theta[3])
   pred[1] = func_sigma2(n_cut,init,eps2[1:n_cut],theta)
   
-  l = n-n_cut
+  l = n-n_cut+1
   for(i in 2:l){
     pred[i] = theta[1]+theta[2]*eps2[n_cut+i-2]+theta[3]*pred[i-1]}
-  return(pred)}
+  return(pred[2:l+1])}
 
 
 rolling_av <- function(eps2,cut){
   
   n = length(eps2)
   n_cut = floor(n*cut)
-  res_temp = rollmean(x=c(rep(NA,9),eps2),k = 10, align='right',na.rm = TRUE)
-  print(res_temp)
-  s = n_cut+1
-  return(res_temp[s:length(res_temp)])}
-
-
-
-l = floor(0.8*length(data$rendement2))
-yy_garch = pred_h1_garch(data$rendement2,0.8)
-plot(data$rendement2[l:length(data$rendement2)],type='l')
-lines(c(1:205),yy_garch, col='red')
-
-plot(data$rendement2[l:length(data$rendement2)],type='l')
-yy_roll = rolling_av(data$rendement2,0.8)
-lines(c(1:205),yy_roll, col='red')
-
-test_mariano(yy_garch,yy_roll,data$rendement2[l:length(data$rendement2)],hor=1)
+  res = rollapply(eps2, width = 10, FUN = mean, align = "right", partial = TRUE)
+  e = length(res)-1
+  return(res[n_cut:e])}
