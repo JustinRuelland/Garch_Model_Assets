533d99ab9b29a4c1c5058c188de9a1ef5b3b8c46
Louis GEIST <louisjjgeist@gmail.com>
2023-03-10 11:22:51 +0100

    study_alpah-beta_sum_constant.R ; initial commit ; alpha

---
+++ study_alpha-beta_sum_constant.R
@@ -0,0 +1,35 @@
+# In the work on the power of the test for GARCH change (puissance_test_chgtGARCH.R), 
+# we found (in the case where the change in training set and test set corresponds to the date of GARCH change), 
+# the existence of a straight line with slope -1 where all pairs (alpha,beta) for the second GARCH 
+# were not rejected by the chi-square test.
+
+# Hence, we focus here on the understanding of the model for two pairs of (alpha,beta) 
+# such that alpha+beta is constant.
+rm(list=ls())
+
+#---------------- import -------------------
+source(file = "./simulation_series.R",local=TRUE)
+#source(file = )
+
+
+
+#-------------- Studied pairs (alpha,beta) ---------------
+n = 1000 #days
+
+omega = 0.0001
+
+theta1 = c(omega,0.12,0.85)
+
+alpha2 = 0.8 #must be included in [0,0,97] (0.97 = alpha1+beta1)
+theta2 = c(omega,alpha2,theta1[2]+theta1[3]-alpha2)
+
+#------------------- simulation ------------------------
+set.seed(2)
+eps_1 = simulation_rendements(n,theta1)
+eps_2 = simulation_rendements(n,theta2)
+
+courbe_prix_arg_rendements(eps_1)
+courbe_prix_arg_rendements(eps_2)
+
+#--------------------- STUDY -------------------
+
