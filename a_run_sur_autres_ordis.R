99d046e170530a95fc6b342efb5138cd16c1e943
Louis Geist <louisjjgeist@gmail.com>
2023-03-20 21:19:17 +0100

    .png ; carte bleue with big n (10 000) ; beta

--- a_run_sur_autres_ordis.R
+++ a_run_sur_autres_ordis.R
@@ -1,4 +1,4 @@
 source(file="./puissance_test_chgtGARCH.R",local=TRUE)
 
 source(file = "./white_noise_laws.R",local=TRUE)
-puissance_test_chgtGARCH(0.8,10,10000,loi_eta = rnorm)
+puissance_test_chgtGARCH(0.4,10,10000,loi_eta = rnorm)
