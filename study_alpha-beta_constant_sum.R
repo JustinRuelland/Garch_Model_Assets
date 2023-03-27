c852ee89908ebb0fa1ef539b6a2b1352ce864ca4
Louis Geist <louisjjgeist@gmail.com>
2023-03-27 09:48:43 +0200

    study_alpha-beta_constant_sum.R ; script in a function and display of some means to better understand the problem ; alpha

--- study_alpha-beta_constant_sum.R
+++ study_alpha-beta_constant_sum.R
@@ -14,72 +14,83 @@ source(file= "./prevision.R", local=TRUE)
 
 
 #-------------- Studied pairs (alpha,beta) ---------------
-# theta1 et theta2 font ici référence aux deux theta étudiés pour la deuxiè
-n = 1000 #days
-cut = 0.8 #cut du backtest ET du changement de GARCH
 
-omega = 0.0001
+#alpha2 must be included in [0,0,97] (0.97 = alpha1+beta1)
+sigma2_hat_alpha_beta_constant <- function(alpha2=0.9, n=1000,seed = 8){
+  # theta1 et theta2 font ici référence aux deux theta étudiés pour la deuxième partie
 
-theta0 = c(omega, 0.12, 0.85)
+  cut = 0.8 #cut du backtest ET du changement de GARCH
+  
+  omega = 0.0001
+  
+  theta0 = c(omega, 0.12, 0.85)
+  
+  theta1 = theta0
+  theta2 = c(omega,alpha2,theta1[2]+theta1[3]-alpha2)
+  
+  #------------------- simulation ------------------------
+  set.seed(seed)
+  etas = rnorm(n)
+  
+  eps_1 = simulation_rendements_avec_changement_GARCH(n,theta_1 = theta0, theta_2 = theta1, cut = cut, etas = etas)
+  eps_2 = simulation_rendements_avec_changement_GARCH(n,theta_1 = theta0, theta_2 = theta2, cut = cut, etas = etas)
+  
+  
+  courbe_prix_arg_rendements(eps_1)
+  courbe_prix_arg_rendements(eps_2)
+  
+  #--------------------- STUDY -------------------
+  pval_1 = func_backtest(eps_1,-1.96,1.96,empirical = FALSE, cut = 0.8)$p.val #Remarque : le QML est refait dans la fonction
+  pval_2 = func_backtest(eps_2,-1.96,1.96,empirical = FALSE, cut = 0.8)$p.val
+  
+  print(pval_1)
+  print(pval_2)
+  
+  n_cut = floor(cut*n)
+  theta1_hat = QML(eps_1[1:n_cut]**2)
+  theta2_hat = QML(eps_2[1:n_cut]**2)
+  
+  sigma1_hat = simu_sigma2(eps_1**2, theta=theta1_hat)
+  sigma2_hat = simu_sigma2(eps_2**2, theta=theta2_hat)
+  
+  # Récupération des vrais sigma2
+  sigma1 = eps_1**2/etas**2
+  sigma2 = eps_2**2/etas**2
+  
+  # Affichages - que de n_cut à n -> test set du backtest
+  window = n_cut:900
+  
+  df = cbind(Date = window,sigma1_hat[window],sigma2_hat[window])
+  df = as.data.frame(df)
+  colnames(df) = c("Date","sigma1_hat","sigma2_hat")
+  
+  
+  df_sigma1_hat = as.data.frame(cbind(Date = window, sigma1_hat[window]))
+  df_sigma2_hat = as.data.frame(cbind(Date = window, sigma2_hat[window]))
+  
+  df_sigma1 = as.data.frame(cbind(Date = window, sigma1[window]))
+  df_sigma2 = as.data.frame(cbind(Date = window, sigma2[window]))
+  
+  # Dataframe pour plot avec des couleurs différentes
+  df_ggplot = bind_rows("Estimation sans changement" = df_sigma1_hat, "Estimation pour changement" = df_sigma2_hat, "Réel sans changement"=df_sigma1,"Réel avec changement"=df_sigma2, .id = "Courbes")
+  
+  print(theta1[2]+theta1[3])
+  print(theta2[2]+theta2[3])
+  
+  p = ggplot(df_ggplot) + geom_line(aes(x=Date, y = V2, color = Courbes)) + ylab("Sigma carré")#+ggtitle("Graphique des sigma carré en fonction du temps\nsans changement de GARCH et avec changement \n(pour alpha+beta constant)")
+  p
+  ggsave("Study_alpha2 = xx.png",width=10,height=5,path="./Graphiques_pour_Latex/Study_ab_constant_sum/")
+  
+  print(paste0("Moyenne sans changement : ",mean(sigma1_hat[n_cut:n])))
+  print(paste0("Moyenne avec changement :",mean(sigma2_hat[n_cut:n])))
 
-theta1 = theta0
+  
+  return(p)
+  }
 
-alpha2 = 0.9 #must be included in [0,0,97] (0.97 = alpha1+beta1)
-theta2 = c(omega,alpha2,theta1[2]+theta1[3]-alpha2)
-#theta2 = c(omega,0.12,0.12)
-
-#------------------- simulation ------------------------
-set.seed(6)
-etas = rnorm(n)
-
-eps_1 = simulation_rendements_avec_changement_GARCH(n,theta_1 = theta0, theta_2 = theta1, cut = cut, etas = etas)
-eps_2 = simulation_rendements_avec_changement_GARCH(n,theta_1 = theta0, theta_2 = theta2, cut = cut, etas = etas)
-
-
-courbe_prix_arg_rendements(eps_1)
-courbe_prix_arg_rendements(eps_2)
-
-#--------------------- STUDY -------------------
-pval_1 = func_backtest(eps_1,-1.96,1.96,empirical = FALSE, cut = 0.8)$p.val #Remarque : le QML est refait dans la fonction
-pval_2 = func_backtest(eps_2,-1.96,1.96,empirical = FALSE, cut = 0.8)$p.val
-
-pval_1
-pval_2
-
-n_cut = floor(cut*n)
-theta1_hat = QML(eps_1[1:n_cut]**2)
-theta2_hat = QML(eps_2[1:n_cut]**2)
-
-sigma1_hat = simu_sigma2(eps_1**2, theta=theta1_hat)
-sigma2_hat = simu_sigma2(eps_2**2, theta=theta2_hat)
-
-# Récupération des vrais sigma2
-sigma1 = eps_1**2/etas**2
-sigma2 = eps_2**2/etas**2
-
-# Affichages - que de n_cut à n -> test set du backtest
-window = n_cut:900
-
-df = cbind(Date = window,sigma1_hat[window],sigma2_hat[window])
-df = as.data.frame(df)
-colnames(df) = c("Date","sigma1_hat","sigma2_hat")
-
-#
-df_sigma1_hat = as.data.frame(cbind(Date = window, sigma1_hat[window]))
-df_sigma2_hat = as.data.frame(cbind(Date = window, sigma2_hat[window]))
-
-df_sigma1 = as.data.frame(cbind(Date = window, sigma1[window]))
-df_sigma2 = as.data.frame(cbind(Date = window, sigma2[window]))
-
-# Dataframe pour plot avec des couleurs différentes
-df_ggplot = bind_rows("Estimation sans changement" = df_sigma1_hat, "Estimation pour changement" = df_sigma2_hat, "Réel sans changement"=df_sigma1,"Réel avec changement"=df_sigma2, .id = "Courbes")
-
-
-p = ggplot(df_ggplot) + geom_line(aes(x=Date, y = V2, color = Courbes)) + ylab("Sigma carré")#+ggtitle("Graphique des sigma carré en fonction du temps\nsans changement de GARCH et avec changement \n(pour alpha+beta constant)")
-p
-ggsave("Study_alpha2 = xx.png",width=10,height=5,path="./Graphiques_pour_Latex/Study_ab_constant_sum/")
+sigma2_hat_alpha_beta_constant(alpha2 = 0.8, seed=16)
 
 # Comparaison des moyennes
-m1 = mean(sigma1_hat[n_cut:n])
-m2 = mean(sigma2_hat[n_cut:n])
+# m1 = mean(sigma1_hat[n_cut:n])
+# m2 = mean(sigma2_hat[n_cut:n])
 # print("blabl" + m1)
