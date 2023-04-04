555b9e3ac5b75d31b7ee0f3559b1a6514b80f54c
Louis Geist <louisjjgeist@gmail.com>
2023-04-04 16:38:37 +0200

    study_alpha-beta_constant_sum.R ; meilleur présentation des moyennes

--- study_alpha-beta_constant_sum.R
+++ study_alpha-beta_constant_sum.R
@@ -16,19 +16,20 @@ source(file= "./prevision.R", local=TRUE)
 #-------------- Studied pairs (alpha,beta) ---------------
 
 #alpha2 must be included in [0,0,97] (0.97 = alpha1+beta1)
-sigma2_hat_alpha_beta_constant <- function(alpha2=0.9, n=1000,seed = 8){
+sigma2_hat_alpha_beta_constant <- function(alpha2=0.9, n=1000,seed = 8, alpha_diff_sum = 0.01, beta_diff_sum = 0.97){
   #--------------- initialisation des paramètres -----------------------
   
   # theta1 et theta2 font ici référence aux deux theta étudiés pour la deuxième partie
 
-  cut = 0.7 #cut du backtest ET du changement de GARCH
+  cut = 0.8 #cut du backtest ET du changement de GARCH
   
   omega = 0.0001
   
-  theta0 = c(omega, 0.12, 0.55)
+  theta0 = c(omega, 0.12, 0.85)
   
-  theta1 = theta0
-  theta2 = c(omega,alpha2,theta1[2]+theta1[3]-alpha2)
+  theta1 = theta0 # pas de changement de la spécification du GARCH(1,1) qui génère les données
+  theta2 = c(omega, alpha2,theta1[2]+theta1[3]-alpha2) # changement, mais qui reste sur la droite alpha+beta constant
+  theta_diff_sum = c(omega, alpha_diff_sum, beta_diff_sum) # changement, qui est en dehors de la droite alpha+beta constant
   
   #------------------- simulation ------------------------
   set.seed(seed)
@@ -36,10 +37,12 @@ sigma2_hat_alpha_beta_constant <- function(alpha2=0.9, n=1000,seed = 8){
   
   eps_1 = simulation_rendements_avec_changement_GARCH(n,theta_1 = theta0, theta_2 = theta1, cut = cut, etas = etas)
   eps_2 = simulation_rendements_avec_changement_GARCH(n,theta_1 = theta0, theta_2 = theta2, cut = cut, etas = etas)
+  eps_3 = simulation_rendements_avec_changement_GARCH(n,theta_1 = theta0, theta_2 = theta_diff_sum, cut = cut, etas = etas)
   
   # Affichage des coubres de prix
-  print(courbe_prix_arg_rendements(eps_1))
+  print(courbe_prix_arg_rendements(eps_1),title="Pas de changement")
   print(courbe_prix_arg_rendements(eps_2))
+  
   
   #--------------------- STUDY -------------------
   # Calcul des p-valeurs (pour comprendre - à ENLEVER à TERME)
@@ -53,13 +56,16 @@ sigma2_hat_alpha_beta_constant <- function(alpha2=0.9, n=1000,seed = 8){
   n_cut = floor(cut*n)
   theta1_hat = QML(eps_1[1:n_cut]**2)
   theta2_hat = QML(eps_2[1:n_cut]**2)
+  theta3_hat = QML(eps_3[1:n_cut]**2)
   
   sigma1_hat = simu_sigma2(eps_1**2, theta=theta1_hat)
   sigma2_hat = simu_sigma2(eps_2**2, theta=theta2_hat)
+  sigma3_hat = simu_sigma2(eps_3**2, theta=theta3_hat)
   
   # Récupération des vrais sigma2
   sigma1 = eps_1**2/etas**2
   sigma2 = eps_2**2/etas**2
+  sigma3 = eps_3**2/etas**2
   
   # Création du graphique final (que de n_cut à n -> test set du backtest)
   window = n_cut:n
@@ -74,35 +80,56 @@ sigma2_hat_alpha_beta_constant <- function(alpha2=0.9, n=1000,seed = 8){
   
   df_sigma1 = as.data.frame(cbind(Date = window, sigma1[window]))
   df_sigma2 = as.data.frame(cbind(Date = window, sigma2[window]))
+  df_sigma3 = as.data.frame(cbind(Data = window, sigma3[window]))
   
   # Dataframe pour plot avec des couleurs différentes
   df_ggplot = bind_rows("Estimation sans changement" = df_sigma1_hat, "Estimation pour changement" = df_sigma2_hat, "Réel sans changement"=df_sigma1,"Réel avec changement"=df_sigma2, .id = "Courbes")
   
-  print(theta1[2]+theta1[3])
-  print(theta2[2]+theta2[3])
+  print(paste0("Cas sans changement : alpha+beta = ",theta1[2]+theta1[3]))
+  print(paste0("Cas avec changement (sur droite) : alpha+beta = ",theta2[2]+theta2[3]))
+  print(paste0("Cas avec changement (hors droite) : alpha+beta =", theta_diff_sum[2]+theta_diff_sum[3]))
   
-  p = ggplot(df_ggplot) + geom_line(aes(x=Date, y = V2, color = Courbes)) + ylab("Sigma carré")#+ggtitle("Graphique des sigma carré en fonction du temps\nsans changement de GARCH et avec changement \n(pour alpha+beta constant)")
+  p = ggplot(df_ggplot) + geom_line(aes(x=Date, y = V2, color = Courbes)) + ylab("Sigma carré") + ggtitle("Comparaison pour alpha+beta constant")#+ggtitle("Graphique des sigma carré en fonction du temps\nsans changement de GARCH et avec changement \n(pour alpha+beta constant)")
   p
   ggsave("Study_alpha2 = xx.png",width=10,height=5,path="./Graphiques_pour_Latex/Study_ab_constant_sum/")
   
-  
+  print("-------------")
+  print(paste0("omega/(1-alpha-beta) = ",theta1[1]/(1-theta1[2]-theta1[3])))
+  print("")
   print(paste0("Moyenne des sigma2 réels sans changement : ",mean(sigma1[n_cut:n])))
   print(paste0("Moyenne des sigma2 réels avec changement : ",mean(sigma2[n_cut:n])))
+  print(paste0("Moyenne des sigma2 réels avec changement (hors droite): ",mean(sigma3[n_cut:n])))
   print("")
   print(paste0("Moyenne des sigma2 estimés sans changement : ",mean(sigma1_hat[n_cut:n])))
   print(paste0("Moyenne des sigma2 estimés avec changement : ",mean(sigma2_hat[n_cut:n])))
+  print(paste0("Moyenne des sigma2 estimés avec changement (hors droite): ",mean(sigma3_hat[n_cut:n])))
   
+  # Graph 2 : Sigma carré réel
+  df_sigma1 = as.data.frame(cbind(Date = window, sigma1[window]))
+  df_sigma2 = as.data.frame(cbind(Date = window, sigma2[window]))
+  df_sigma3 = as.data.frame(cbind(Date = window, sigma3[window]))
   
-  # TESTSSS
-  plot(eps_1**2)
-  plot(eps_2**2)
-  print(mean(eps_1[window]**2))
-  print(mean(eps_2[window]**2))
-  print(theta1_hat)
+  df_ggplot_reel = bind_rows("Réel sans changement"=df_sigma1,"Réel avec changement"=df_sigma2,"Réel avec changement hors de la droite"=df_sigma3, .id = "Courbes")
+  plot_reel = ggplot(df_ggplot_reel) + geom_line(aes(x = Date, y=V2, color= Courbes))+ ylab("Sigma carré") +ggtitle("Graphe des sigma2 réel")
   
+  print(plot_reel)
   return(p)
-  }
+}
 
-sigma2_hat_alpha_beta_constant(alpha2 = 0.5, seed=3012, n = 10000)
+
+moyenne <-function (n=1000){
+  
+  set.seed(seed)
+  etas = rnorm(n)
+  
+  eps_1 = simulation_rendements_avec_changement_GARCH(n,theta_1 = theta0, theta_2 = theta1, cut = cut, etas = etas)
+  eps_2 = simulation_rendements_avec_changement_GARCH(n,theta_1 = theta0, theta_2 = theta2, cut = cut, etas = etas)
+  
+  return()
+  
+}
+
+
+#sigma2_hat_alpha_beta_constant(alpha2 = 0.8, seed=310, n = 1000)
 # Problème : la moyenne des sigma2 (les vrais déjà !!) de la série 
 # avec changement est toujours environ la moitiée de la série sans changement
