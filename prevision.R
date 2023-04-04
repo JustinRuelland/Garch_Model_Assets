d52487bc9cffabe727adafcaa5593d67eaf7c569
Justin RUELLAND <justinruelland@icloud.com>
2023-04-04 16:21:10 +0200

    Ajout d'une fonction de backtesting sur les epsilons squared bivarie

--- prevision.R
+++ prevision.R
@@ -40,46 +40,6 @@ func_backtest <- function(eps,q_inf,q_sup,cut,empirical){
 
   return(res)}
 
-#func_backtest <- function(eps,q_inf,q_sup,cut,empirical){
-  
-  n = length(eps)
-  n_cut = floor(cut*n)
-  eps2 = eps**2
-  
-  #QML, on s'attend à ce que la fonction ait été importée
-  theta = QML(eps2[c(1:n_cut)]) #estimation de theta par QML sur les données jusqu'au "cut"
-  sigma2 = simu_sigma2(eps2,theta) #estimation des sigma2 à partir des epsilon2 passés
-  sigma = sqrt(sigma2)
-  eta_quantile = eps[1:n_cut]/sigma[1:n_cut]
-  
-  #tests intervalle de confiance
-  n_cut = n_cut+1
-  eps_test = eps[n_cut:n]
-  sigma_test = sigma[n_cut:n]
-  
-  
-  if(empirical==FALSE){
-    upper = q_sup*sigma_test
-    lower = q_inf*sigma_test}
-  else{
-    upper = quantile(x=eta_quantile,prob=0.975)*sigma_test
-    lower = quantile(x=eta_quantile,prob=0.025)*sigma_test}
-  
-  outside = length(eps_test[(eps_test>upper)|(eps_test<lower)])
-  inside = n - n_cut + 1 -outside
-  
-  #test d'adéquation du khi2 sur les Bernoulli (paramètre 0.95)
-  obs = c(outside,inside) #effectifs observés
-  proba = c(0.05,0.95) #probabilités théoriques
-  
-  res = matrix(0,ncol=3)
-  colnames(res) = c("p.value","upper.bounds","lower.bounds")
-  res$p.value = chisq.test(obs,p=proba)$p.value
-  res$upper.bounds = upper
-  res$lower.bounds = lower
-  
-  return(res)
-}
 
 #hypothèse: les etas suivent une loi normale
 backtest_square_modified <-function(eps2,q_inf, q_sup, cut, empirical){
